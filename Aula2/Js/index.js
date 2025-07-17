5bc81cc9282802a80869d0e59823f39105571186
Luan <castro.luanjfmg@gmail.com>
2025-07-17 09:28:52 -0300

    Somando números com javaScript e mostrando no documento Html
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

---
+++ Aula2/Js/index.js
@@ -0,0 +1,9 @@
+let a, b, c;
+
+a = 100;
+b = 1;
+c = 50;  
+
+let soma = a + b + c;
+
+document.getElementById("resultado").innerHTML = "A soma dos elementos declarados dentro do documento javaScript é: " + soma; 
