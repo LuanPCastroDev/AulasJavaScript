62ab17d41fdc568583998f4d6ba4cedf5d536320
Luan <castro.luanjfmg@gmail.com>
2025-07-17 13:05:07 -0300

    Trabalhando com variáveis utilizando o let
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula3-Variáveis/Js/index.js
+++ Aula3-Variáveis/Js/index.js
@@ -1,8 +1,19 @@
 //declaração de variáveis
 var a,b,c;
 
+
+//Atribuição de variáveis
 a = 9;
 b = 10;
 c = a + b;
 
 alert(c);
+
+let d, e, f;
+
+d = (5 * 9);
+e = (10 * 80);
+f = d + e;
+
+
+document.getElementById("soma").innerHTML = "A soma dos elementos é: " + f;
