db7d96f77fd12030b55d9dfdfc8bd825e1efe176
Luan <castro.luanjfmg@gmail.com>
2025-07-17 09:55:47 -0300

    Identando documento index.js
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula2/Js/index.js
+++ Aula2/Js/index.js
@@ -1,24 +1,23 @@
 let a, b, c;
-
 a = 100;
 b = 1;
 c = 50;  
-
 let soma = a + b + c;
+
 document.getElementById("resultado").innerHTML = "A soma dos elementos declarados dentro do documento javaScript é: " + soma; 
 
 let numeros;
- 
 numeros = ("5 + 5");
+
 document.getElementById("exemplo1").innerHTML = "Quando escrevemos dois números dentro de aspas duplas o javaScript não entende isso como uma soma, mas como um texto. Como nesse exemplo: " + numeros;
 
 let somadenumeros;
 somadenumeros = ( 2 + 8 );
+
 document.getElementById("exemplo2").innerHTML = "Quando escrevemos dois números dentro de parênteses, mas sem aspas duplas o javaScript entende isso como uma soma. Como nesse exemplo: " + somadenumeros;
 
 let nome;
 let idade;
-
 nome = "Luan";
 idade = "34";
 
