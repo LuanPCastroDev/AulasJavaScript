9a5d3a9a6756624ccc4e6565736809e7cbf5304c
Luan <castro.luanjfmg@gmail.com>
2025-07-17 09:43:16 -0300

    Trabalhando com sintaxe em JavaScript
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula2/Js/index.js
+++ Aula2/Js/index.js
@@ -7,3 +7,8 @@ c = 50;
 let soma = a + b + c;
 
 document.getElementById("resultado").innerHTML = "A soma dos elementos declarados dentro do documento javaScript é: " + soma; 
+
+let numeros;
+ 
+numeros = ("5 + 5");
+document.getElementById("exemplo1").innerHTML = "Quando escrevemos dois números dentro de aspas duplas o javaScript não entende isso como uma soma, mas como um texto. Como nesse exemplo: " + numeros;
