f7bf9723009c96749dd8d3d1a184bece0dd4cf45
Luan <castro.luanjfmg@gmail.com>
2025-07-18 10:40:38 -0300

    Trabalhando com os operadores básicos e mostrando o resultado na página html
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula4-Operadores/Js/index.js
+++ Aula4-Operadores/Js/index.js
@@ -4,3 +4,12 @@ valor2 = 2;
 
 total = valor1 + valor2;
 document.getElementById("resultado").innerHTML = "o resultado da soma é =  " + total;
+
+totalsubtracao = valor1 - valor2;
+document.getElementById("resultadosubtracao").innerHTML = "o resultado da subtração é =  " + totalsubtracao;
+
+totaldivisao = valor1 / valor2;
+document.getElementById("resultadodivisao").innerHTML = "o resultado da divisão é =  " + totaldivisao;
+
+totalmultiplicacao = valor1 * valor2;
+document.getElementById("resultadomultiplicacao").innerHTML = "o resultado da multiplicação é =  " + totalmultiplicacao;
