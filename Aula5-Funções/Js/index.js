784787e52195672b533d4c0b478592016a278c50
Luan <castro.luanjfmg@gmail.com>
2025-07-19 11:15:14 -0300

    Criando função de divisão e para converter valor de real em Dólar
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula5-Funções/Js/index.js
+++ Aula5-Funções/Js/index.js
@@ -8,3 +8,18 @@ function subtracao (valor1, valor2) {
     return valor1 - valor2;
 }
 document.getElementById("funcaosubtracao").innerHTML = "O resultado da subtração é: " + subtracao(150,200);
+
+function divisao (valor1, valor2) {
+    return valor1 / valor2;
+}
+document.getElementById("funcaodivisao").innerHTML = "  O resultado da divisão é: " + divisao(148,1000);
+
+function converterDolar (real, dolar) {
+    return real * dolar;
+}
+
+var valorReal = 10.98;
+var cotacaoDolar = 5.58;
+var resultado = converterDolar(valorReal, cotacaoDolar);
+
+document.getElementById("realdolarconversao").innerHTML = "O resultado da conversão em dólar é: R$ " + resultado; 
