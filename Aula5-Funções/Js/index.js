e909f9de2a6062062e4fc4ed4a23fd8faf8d74c2
Luan <castro.luanjfmg@gmail.com>
2025-07-19 10:51:00 -0300

    Criando uma função para soma e subtração e mostrando resultado na página HTML
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

---
+++ Aula5-Funções/Js/index.js
@@ -0,0 +1,10 @@
+function soma (valor1, valor2) {
+    return valor1 + valor2;
+}
+
+document.getElementById("funcaosoma").innerHTML = "O resultado da soma é: " + soma(150,200);
+
+function subtracao (valor1, valor2) {
+    return valor1 - valor2;
+}
+document.getElementById("funcaosubtracao").innerHTML = "O resultado da subtração é: " + subtracao(150,200);
