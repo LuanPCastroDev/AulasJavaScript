1dba7b3da6f4c4665eb531b93f7c327d4f517512
Luan <castro.luanjfmg@gmail.com>
2025-07-21 09:33:35 -0300

    Inserindo dados no objeto carro
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

---
+++ Aula6-Objetos/js/index.js
@@ -0,0 +1,94 @@
+const carros = [
+  {
+    marca: "Toyota",
+    modelo: "Corolla",
+    ano: 2020,
+    cor: "Prata",
+    tipo: "Sedan",
+    motor: "2.0",
+    combustivel: "Gasolina"
+  },
+  {
+    marca: "Honda",
+    modelo: "Civic",
+    ano: 2019,
+    cor: "Preto",
+    tipo: "Sedan",
+    motor: "1.5",
+    combustivel: "Gasolina"
+  },
+  {
+    marca: "Ford",
+    modelo: "Fusion",
+    ano: 2021,
+    cor: "Azul",
+    tipo: "Sedan",
+    motor: "2.5",
+    combustivel: "Flex"
+  },
+  {
+    marca: "Chevrolet",
+    modelo: "Cruze",
+    ano: 2018,
+    cor: "Branco",
+    tipo: "Sedan",
+    motor: "1.4",
+    combustivel: "Gasolina"
+  },
+  {
+    marca: "Volkswagen",
+    modelo: "Jetta",
+    ano: 2020,
+    cor: "Vermelho",
+    tipo: "Sedan",
+    motor: "1.4",
+    combustivel: "Gasolina"
+  },
+  {
+    marca: "BMW",
+    modelo: "320i",
+    ano: 2022,
+    cor: "Cinza",
+    tipo: "Sedan",
+    motor: "2.0",
+    combustivel: "Gasolina"
+  },
+  {
+    marca: "Audi",
+    modelo: "A4",
+    ano: 2021,
+    cor: "Preto",
+    tipo: "Sedan",
+    motor: "2.0",
+    combustivel: "Gasolina"
+  },
+  {
+    marca: "Mercedes-Benz",
+    modelo: "C-Class",
+    ano: 2022,
+    cor: "Branco",
+    tipo: "Sedan",
+    motor: "2.0",
+    combustivel: "Gasolina"
+  },
+  {
+    marca: "Nissan",
+    modelo: "Altima",
+    ano: 2020,
+    cor: "Prata",
+    tipo: "Sedan",
+    motor: "2.5",
+    combustivel: "Gasolina"
+  },
+  {
+    marca: "Hyundai",
+    modelo: "Elantra",
+    ano: 2019,
+    cor: "Azul",
+    tipo: "Sedan",
+    motor: "2.0",
+    combustivel: "Gasolina"
+  }
+];
+
+console.log(carros);
