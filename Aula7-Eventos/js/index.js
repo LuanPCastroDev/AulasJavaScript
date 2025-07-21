89f08335806ad42cb6ef3ee434b704af82e27e6a
Luan <castro.luanjfmg@gmail.com>
2025-07-21 10:31:25 -0300

    Mudando cor de página ao clicar no botão.
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula7-Eventos/js/index.js
+++ Aula7-Eventos/js/index.js
@@ -1,3 +1,5 @@
 function eventoclick () {
     alert('Você clicou no botão clique aqui');
+    document.body.style.backgroundColor = "yellow";
+    //Ao clicar no botão o corpo de fundo da página fica amarelo
 }
