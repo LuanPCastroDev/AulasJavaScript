d0c46fbb0344b68d570692b28640ae1ef0e4606e
Luan <castro.luanjfmg@gmail.com>
2025-07-21 10:36:28 -0300

    Innserindo função de clique duplo e mudando a cor do fundo da tela ao realizar o clique duplo
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula7-Eventos/js/index.js
+++ Aula7-Eventos/js/index.js
@@ -1,5 +1,10 @@
 function eventoclick () {
-    alert('Você clicou no botão clique aqui');
+    alert('Você clicou no botão.');
     document.body.style.backgroundColor = "yellow";
     //Ao clicar no botão o corpo de fundo da página fica amarelo
+}
+
+function eventoDblClick () {
+    alert("Você realizou um clique duplo no botão.");
+    document.body.style.backgroundColor = "blue";
 }
