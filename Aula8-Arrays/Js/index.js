1cac52419cc49a13655b87dce7c1e3b9fe4c1120
Luan <castro.luanjfmg@gmail.com>
2025-07-22 10:30:13 -0300

    Imprimindo produtos da lista da matriz
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula8-Arrays/Js/index.js
+++ Aula8-Arrays/Js/index.js
@@ -5,3 +5,11 @@ var item4 = "leite";
 
 const listadecompras = [ "arroz", "feijão", "macarrão", "leite", "frango", "toddy", "veja", "detergente" ];
 
+document.getElementById("produto").innerHTML = "Produto da lista: " + listadecompras[0];
+document.getElementById("produto1").innerHTML = "Produto da lista: " + listadecompras[1];
+document.getElementById("produto2").innerHTML = "Produto da lista: " + listadecompras[2];
+document.getElementById("produto3").innerHTML = "Produto da lista: " + listadecompras[3];
+document.getElementById("produto4").innerHTML = "Produto da lista: " + listadecompras[4];
+document.getElementById("produto5").innerHTML = "Produto da lista: " + listadecompras[5];
+document.getElementById("produto6").innerHTML = "Produto da lista: " + listadecompras[6];
+document.getElementById("produto7").innerHTML = "Produto da lista: " + listadecompras[7];
