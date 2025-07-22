d73f47633747443b35837bae8d9f9c28b7eb2191
Luan <castro.luanjfmg@gmail.com>
2025-07-22 10:25:32 -0300

    Criando itens de um vetor em javascript
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

---
+++ Aula8-Arrays/Js/index.js
@@ -0,0 +1,7 @@
+var item1 = "arroz";
+var item2 = "feijão";
+var item3 = "macarrão";
+var item4 = "leite";
+
+const listadecompras = [ "arroz", "feijão", "macarrão", "leite", "frango", "toddy", "veja", "detergente" ];
+
