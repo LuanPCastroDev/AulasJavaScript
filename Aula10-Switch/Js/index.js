7883b01625660c9ab55b01245ad4eb02ba3c5c7f
Luan <castro.luanjfmg@gmail.com>
2025-07-24 10:08:35 -0300

    Inserindo switch para verificar cor digitada.
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula10-Switch/Js/index.js
+++ Aula10-Switch/Js/index.js
@@ -1,3 +1,22 @@
 function verificaCor() {
-    
+    let cor = document.getElementById("cor").value;
+
+    switch (cor) {
+        case "azul":
+            document.body.style.backgroundColor = "blue";
+            break;
+        case "vermelho":
+            document.body.style.backgroundColor = "red";
+            break;
+        case "amarelo":
+            document.body.style.backgroundColor = "yellow";
+            break;
+        case "verde":
+            document.body.style.backgroundColor = "green";
+            break;
+        default:
+            document.getElementById("teste").innerHTML = "Nenhuma cor disponível"        
+        
+    }
 }
+
