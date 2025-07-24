5cca21f825ec1e77353a16f2e4e729b7bc0b05f6
Luan <castro.luanjfmg@gmail.com>
2025-07-24 13:46:50 -0300

    Construindo select com opções de ano para selecionar
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula11-For/Js/index.js
+++ Aula11-For/Js/index.js
@@ -5,3 +5,7 @@ for (let i = 0; i <= 1000; i++) {
 for (let j = 1000; j >= 0; j--) {
     document.getElementById("repeticaoReversa").innerHTML += j + ", ";
 }
+
+for (let i = 1900; i <= 2025; i++) {
+    document.getElementById("ano").innerHTML += "<option value =  " + i + " ";
+}
