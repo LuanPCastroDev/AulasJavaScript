0af8e581834196dea25df05e48fe9dd50ba88e1a
Luan <castro.luanjfmg@gmail.com>
2025-07-24 13:42:46 -0300

    Escrevendo números de forma decrescente de 1000 até 0
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula11-For/Js/index.js
+++ Aula11-For/Js/index.js
@@ -1,3 +1,7 @@
 for (let i = 0; i <= 1000; i++) {
-    document.getElementById("repeticaoteste").innerHTML += i + ", "; 
+    document.getElementById("repeticaoTeste").innerHTML += i + ", "; 
+}
+
+for (let j = 1000; j >= 0; j--) {
+    document.getElementById("repeticaoReversa").innerHTML += j + ", ";
 }
