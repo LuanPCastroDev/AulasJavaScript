8935070e4732bdac9db0e310a6a6bd658e08c36f
Luan <castro.luanjfmg@gmail.com>
2025-07-25 14:14:16 -0300

    Criando função setInterval e botão do cronômetro
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula12/Js/index.js
+++ Aula12/Js/index.js
@@ -3,6 +3,13 @@ function ativarContagem() {
     tempo = setTimeout( function() {
         document.getElementById('tempo').innerHTML = "Executou o setTimeout.";
     }, 5000);
+
+
+    setInterval( function () {
+        var cronometro = document.getElementById('tempo').innerHTML;
+        var soma = parseInt(cronometro) + 1;
+        document.getElementById('tempo').innerHTML = soma;
+    }, 1000);
 }
  
 function pararContagem() {
