e7ff48c9253c3f61f1a7656a9ab4b5c80569a03f
Luan <castro.luanjfmg@gmail.com>
2025-07-25 14:16:55 -0300

    Inserindo função de cronômetro
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula12/Js/index.js
+++ Aula12/Js/index.js
@@ -3,16 +3,17 @@ function ativarContagem() {
     tempo = setTimeout( function() {
         document.getElementById('tempo').innerHTML = "Executou o setTimeout.";
     }, 5000);
-
-
-    setInterval( function () {
-        var cronometro = document.getElementById('tempo').innerHTML;
-        var soma = parseInt(cronometro) + 1;
-        document.getElementById('tempo').innerHTML = soma;
-    }, 1000);
 }
  
 function pararContagem() {
     clearTimeout(tempo);
     document.getElementById('tempo').innerHTML = "Parou a contagem.";
+}
+
+function cronometro () {
+    setInterval( function () {
+        var cronometro = document.getElementById('tempo').innerHTML;
+        var soma = parseInt(cronometro) + 1;
+        document.getElementById('tempo').innerHTML = soma;
+    }, 1000);
 }
