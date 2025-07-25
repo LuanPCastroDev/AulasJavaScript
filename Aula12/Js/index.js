eb46ad2777445ddcd6e577f24fe4aff483a36e76
Luan <castro.luanjfmg@gmail.com>
2025-07-25 14:08:33 -0300

    Função setTimeout
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula12/Js/index.js
+++ Aula12/Js/index.js
@@ -1,6 +1,11 @@
 function ativarContagem() {
-    document.getElementById('tempo').innerHTML = "Começou a contar";
-    setTimeout( function() {
-        document.getElementById('tempo').innerHTML = "Executou o setTimeout";
+    document.getElementById('tempo').innerHTML = "Começou a contar.";
+    tempo = setTimeout( function() {
+        document.getElementById('tempo').innerHTML = "Executou o setTimeout.";
     }, 5000);
+}
+ 
+function pararContagem() {
+    clearTimeout(tempo);
+    document.getElementById('tempo').innerHTML = "Parou a contagem.";
 }
