73670c1d0e9c51f70a8e88c24d9a2eb1b0dc137b
Luan <castro.luanjfmg@gmail.com>
2025-07-28 10:11:48 -0300

    Corrigindo erro de sintaxe na classe Carro e imprimindo carro na página HTML
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- Aula13-Classes/Js/index.js
+++ Aula13-Classes/Js/index.js
@@ -1,4 +1,4 @@
-class carro {
+class Carro {
     constructor (valor1, valor2, valor3, valor4) {
         this.marca = valor1;
         this.modelo = valor2;
@@ -7,9 +7,14 @@ class carro {
     }
 }
 
-const carro = {
-    marca: "Renault",
-    modelo: "Stepwway",
-    ano: "2019-2020",
-    motor: "1.6",
-}
+const sandero = new Carro("Renault", "Stepway", "2019-2020", "1.6");
+const polo = new Carro("Volkswagen", "Polo", "2024-2025", "1.6");
+
+document.getElementById("carro1").innerHTML = 
+"Carro 1 = Marca: " + sandero.marca +
+", Modelo: " + sandero.modelo +
+", Ano: " + sandero.ano +
+", Motor: " + sandero.motor;
+
+console.log(sandero);
+console.log(polo);
