9fe1d656acb9d9167bfcbb7a6c6de62d179eec32
Luan <castro.luanjfmg@gmail.com>
2025-07-28 09:58:01 -0300

    criando constructor carro e const carro
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

---
+++ Aula13-Classes/Js/index.js
@@ -0,0 +1,15 @@
+class carro {
+    constructor (valor1, valor2, valor3, valor4) {
+        this.marca = valor1;
+        this.modelo = valor2;
+        this.ano = valor3;
+        this.motor = valor4;
+    }
+}
+
+const carro = {
+    marca: "Renault",
+    modelo: "Stepwway",
+    ano: "2019-2020",
+    motor: "1.6",
+}
